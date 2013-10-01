dd994147b8943219730adcc4d19e48e7dd6fbca3
udaygarud <udayraj007@gmail.com>
2013-10-01 19:41:20 +0530

    my first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpwork::Application.config.secret_token = '004848b48d88a657a351ac31b8204e31c19b8b9ae30ebdfefb4f77f26753a264818719d17ad2092dd1cf6cc530b73cbc8aef7a9d42df4f37008f0b439cf4d36f'
